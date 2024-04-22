1422202b2166c240fa3393f1030532e6cdd54dfb
kubuszi <kubushii2@wp.pl>
2024-04-22 11:14:17 +0200

    Create food class and detect colissions with food object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,7 +21,9 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -7,11 +7,13 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             new_segment = Turtle("square")
