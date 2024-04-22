cfc4b65ff057690492756c11ea07dd2a27971349
kubuszi <kubushii2@wp.pl>
2024-04-22 10:57:04 +0200

    Create screen game and snake class

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Screen, Turtle
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
