67be8c4de8979e38a9394cab1f1f94e5488b0bc7
kubuszi <kubushii2@wp.pl>
2024-04-23 11:42:16 +0200

    Detect collisions with tail and walls

--- main.py
+++ main.py
@@ -26,7 +26,18 @@ while game_on:
     snake.move()
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
